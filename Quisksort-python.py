cb6fe344fce2e0fc9f95e1f0a969a32b43943498
Pulkit Nehra <65600727+pulkit-nehra05@users.noreply.github.com>
2020-06-18 11:02:01 +0530

    Update Quisksort-python.py

--- Quisksort-python.py
+++ Quisksort-python.py
@@ -4,19 +4,21 @@ def quickSort(arr):
     quickSrt(arr, 0, len(arr) - 1)
     return arr
 
-
 def quickSrt(arr, left, right):
     i = left
     j = right
     mid = (left + right) // 2
     # Find the pivot
     pivot = arr[mid]
+    
     # traverse the array till its end
     while i <= j:
         # divide the array into left and right subarray on basis of pivot
+        
         # elements less than pivot
         while arr[i] < pivot:
             i += 1
+            
         # elements greater than the pivot
         while arr[j] > pivot:
             j -= 1
@@ -28,7 +30,8 @@ def quickSrt(arr, left, right):
             But there can exist a condition where arr[i] > pivot even before the index i < pivot
             In this case it will exit the loop and will create problems in sorting at the left subarray. 
             Same is the case with the right subarray where arr[j] < pivot
-            """
+        """
+        
         # To counter this we create a pass condition which will save us the hassle
         if i <= j:
             # swap the positions of elements at index i and j
@@ -38,12 +41,12 @@ def quickSrt(arr, left, right):
             i += 1
             j -= 1
 
-            # After this condition is executed, we will get our perfect arrangement
+        # After this condition is executed, we will get our perfect arrangement
 
-            """
+        """
             But, there is one more problem both the left and right subarray could or will contain
             elements that are unsorted. So we sort them and get the resultant sorted array
-            """
+        """
 
         # Sorting left sub-array recursively
         if left < j:
